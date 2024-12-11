4052f7ed6531bcd592462c89f7c3ae701e7c8963
yangxin <981987024@qq.com>
2024-12-11 12:19:11 +0800

    first commit

---
+++ src/main/java/com/belacloud/aliyunserverlessdemo/AliyunServerlessDemoApplication.java
@@ -0,0 +1,13 @@
+package com.belacloud.aliyunserverlessdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyunServerlessDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunServerlessDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/belacloud/aliyunserverlessdemo/AliyunServerlessDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belacloud.aliyunserverlessdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyunServerlessDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
